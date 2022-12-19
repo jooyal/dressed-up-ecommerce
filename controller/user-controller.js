3224a11d31da32ecc07f98d6a6dccc71ca9b26ce
jojoyal <josepjoyal@gmail.com>
2022-12-19 15:21:33 +0530

    added some pages

---
+++ controller/user-controller.js
@@ -0,0 +1,23 @@
+
+
+
+module.exports = {
+    landingPage : (req, res, next)=> {
+        res.render('userView/landing-page', { title: 'Explore Latest Styles For You and your Home - Dressed Up'});
+    },
+    signupPage : (req,res)=> {
+        res.render('userView/signup', {title: 'Create an account | Dressed Up'})
+    },
+    loginPage : (req,res)=> {
+        res.render('userView/login',{title:'Log In to your Account | Dressed Up'})
+    },
+    otpLogin : (req,res)=> {
+        res.render('userView/OTP-login',{title:'Log In to your Account | Dressed Up'})
+    },
+    otpLoginVerification : (req,res)=> {
+        res.render('userView/OTP-login-verification',{title:'Enter the One Time Password sent to your account.'})
+    },
+    userHome : (req,res)=> {
+        res.render('userView/home', { title: 'Explore Latest Styles For You and your Home - Dressed Up'});
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {landingPage, signupPage, loginPage, otpLogin, otpLoginVerification, userHome } = require('../controller/user-controller')
 
 /* GET home page. */
-router.get('/',(req, res, next)=> {
-  res.render('userView/home', { title: 'Explore Latest Styles For You and your Home - Dressed Up'});
-});
+router.get('/',landingPage);
+router.get('/home',userHome)
+router.get('/signup', signupPage)
+router.get('/login',loginPage)
+router.get('/OTP-login',otpLogin)
+router.get('/OTP-login-verification',otpLoginVerification)
+
 
 module.exports = router;
