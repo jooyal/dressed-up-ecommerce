73660763e60193955ef9552cf34e1370fa00a009
jojoyal <josepjoyal@gmail.com>
2023-01-29 22:04:24 +0530

    pdf rendering fixed

--- utilities/pdfService.js
+++ utilities/pdfService.js
@@ -3,6 +3,7 @@ const path = require('path')
 const parentDir = (path.resolve(__dirname, '..'));
 
 function buildPDF(dataToWrite, dataCallback, endCallBack) {
+  console.log(dataToWrite);
     const doc = new PDFDocument();
     doc.on('data', dataCallback)
     doc.on('end', endCallBack)
@@ -24,14 +25,16 @@ function buildPDF(dataToWrite, dataCallback, endCallBack) {
 
     doc.fontSize(14)
    .text('Total number of sales today : '+ dataToWrite.salesNumberToday)
-   .text('Total revenue from sales today : '+ dataToWrite.salesAmountToday);
+   .text('Total revenue from sales today : '+ dataToWrite.salesAmountToday.total)
+   .text('Total GST collected today : '+ dataToWrite.salesAmountToday.tax);
 
    doc.moveDown();
    doc.moveDown();
 
    doc.fontSize(14)
    .text('Total number of sales this month : '+ dataToWrite.salesNumberMonth)
-   .text('Total revenue from sales this month : '+ dataToWrite.salesAmountMonth);
+   .text('Total revenue from sales this month : '+ dataToWrite.salesAmountMonth.total)
+   .text('Total GST collected this month : '+ dataToWrite.salesAmountMonth.tax);
 
 //    doc.font('Helvetica').fontSize(14)
 //    .text(totalSalesToday, doc.x + (doc.page.width - 100) - 20, doc.y + 20, { align: 'right'})
